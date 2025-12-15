91e15660a6076e8c8b22b1cfa7b08abd89ecea6b
raulpereira-01 <therokin3@gmail.com>
2025-12-15 01:02:35 +0100

    Fix dashboard settings toggles

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:finance_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Onboarding welcome screen is displayed',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const FinanceApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Welcome 👋'), findsOneWidget);
+    expect(find.text('Let’s set up your finances in a few simple steps.'),
+        findsOneWidget);
+    expect(find.text('Start'), findsOneWidget);
   });
 }
