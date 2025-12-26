5a9851f170d52e8c57de0b95812a96ea2539d649
raulpereira-01 <therokin3@gmail.com>
2025-12-26 02:14:28 +0100

    Fix local_auth authenticate options

--- lib/presentation/widgets/authentication_gate.dart
+++ lib/presentation/widgets/authentication_gate.dart
@@ -56,9 +56,11 @@ class _AuthenticationGateState extends State<AuthenticationGate> {
 
       final didAuthenticate = await _localAuth.authenticate(
         localizedReason: AppLocalizations.of(context).authenticationMessage,
-        useErrorDialogs: true,
-        stickyAuth: true,
-        biometricOnly: false,
+        options: const AuthenticationOptions(
+          biometricOnly: false,
+          useErrorDialogs: true,
+          stickyAuth: true,
+        ),
       );
 
       if (!mounted) return;
